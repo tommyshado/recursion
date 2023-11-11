0b4edec2fa0050ef366010c6adcc4b4e54283d9f
Mthunzi Tom <107440311+tommyshado@users.noreply.github.com>
2023-11-11 22:44:13 +0200

    Update is-palindrome.js function

--- 12-recursion-i/07-is-palindrome/is-palindrome.js
+++ 12-recursion-i/07-is-palindrome/is-palindrome.js
@@ -1,45 +1,18 @@
 // YOUR CODE BELOW
 
-// function isPalindrome(aStr) {
+function isPalindrome(aStr) {
     
+    if (aStr.length <= 1) {
+        return true;
+    };
 
-//     if (aStr.length <= 1) {
-//         return true;
-//     }
+    const firstCharacter = aStr[0];
+    const lastCharacter = aStr[aStr.length - 1];
 
-//     const firstCharacter = aStr[0];
-//     const lastCharacter = aStr[aStr.length - 1];
+    if (firstCharacter === lastCharacter) {
+        return isPalindrome(aStr.slice(1, -1));
+    } else {
+        return false;
+    };
 
-//     if (firstCharacter === lastCharacter) {
-//         return isPalindrome(aStr.slice(1, -1));
-//     } else {
-//         return false;
-//     }
-
-// };
-
-// console.log(isPalindrome("race"));
-
-
-
-
-function flattenArray(nestedArray) {
-    let flatArray = 0;
-
-    for (let i = 0; i < nestedArray.length; i++) {
-        const element = nestedArray[i];
-
-        if (Array.isArray(element)) {
-            flatArray += flattenArray(element);
-        } else {
-            flatArray += element;
-        }
-    }
-
-    return flatArray;
-}
-
-
-let array = [1, [2, 3, [4, 5, 6, [7, 8, 9 ]]]]
-
-console.log(flattenArray(array));
+};
